eb39bef57dc2c46be7b7ddd02e539651f26c2f7a
Dokotela <grey.faulkenberry@mayjuun.com>
2024-07-13 17:55:18 -0400

    little updates for dart

--- dart/upload_all.dart
+++ dart/upload_all.dart
@@ -4,18 +4,13 @@ import 'package:fhir_r5/fhir_r5.dart';
 import 'package:pocketbase/pocketbase.dart';
 
 Future<void> main() async {
-  final PocketBase pb = PocketBase('http://127.0.0.1:8090');
+  final PocketBase pb = PocketBase('https://pocket-fhir.fly.dev');
 
   // Authenticate
-  await pb.admins.authWithPassword('grey@fhirfli.dev', '01 password');
+  await pb.admins.authWithPassword('grey@fhirfli.dev', '');
 
-  // await assets(pb);
-  // await examples(pb);
-  await mimic(pb);
+  await assets(pb);
 }
-
-final String pocketbaseId = '4260a7372075e64';
-//PocketbaseId.generate();
 
 Future<void> assets(PocketBase pb) async {
   final Directory dir1 = Directory('assets');
@@ -23,10 +18,18 @@ Future<void> assets(PocketBase pb) async {
   for (final FileSystemEntity file in files) {
     if (file is File) {
       print(file.path);
-      final Bundle bundle = Bundle.fromJsonString(await file.readAsString());
-      for (final BundleEntry entry in bundle.entry ?? <BundleEntry>[]) {
-        final resource = entry.resource;
-        if (resource != null) {
+      if (file.path.endsWith('.json')) {
+        final Bundle bundle = Bundle.fromJsonString(file.readAsStringSync());
+        for (final BundleEntry entry in bundle.entry ?? <BundleEntry>[]) {
+          final resource = entry.resource;
+          if (resource != null) {
+            await createOrUpdateRecord(pb, resource.toJson());
+          }
+        }
+      } else if (file.path.endsWith('.ndjson')) {
+        final List<String> lines = file.readAsLinesSync();
+        for (final String line in lines) {
+          final Resource resource = Resource.fromJsonString(line);
           await createOrUpdateRecord(pb, resource.toJson());
         }
       }
@@ -43,26 +46,6 @@ Future<void> examples(PocketBase pb) async {
       final Resource resource =
           Resource.fromJsonString(await file.readAsString());
       await createOrUpdateRecord(pb, resource.toJson());
-    }
-  }
-}
-
-Future<void> mimic(PocketBase pb) async {
-  final Directory dir3 = Directory('mimic-fhir');
-  final List<FileSystemEntity> files3 = dir3.listSync();
-  for (final FileSystemEntity file in files3) {
-    if (file is File) {
-      print(file.path);
-      final String fileString = await file.readAsString();
-      final List<String> lines = fileString.split('\n');
-      for (final String line in lines) {
-        try {
-          final Resource resource = Resource.fromJsonString(line);
-          await createOrUpdateRecord(pb, resource.toJson());
-        } catch (e) {
-          print(e);
-        }
-      }
     }
   }
 }
