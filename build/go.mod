f5fffd9bc09d896fb960f5c9fead31b6355ec303
Dokotela <grey.faulkenberry@mayjuun.com>
2024-07-20 13:02:34 -0400

    temp

---
+++ build/go.mod
@@ -0,0 +1,3 @@
+module pocketfhir
+
+go 1.22.4
--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
 )
 
 func main() {
@@ -15,10 +16,26 @@ func main() {
 	// Register hooks from hooks.go
 	registerHooks(app)
 
-	// Initialize collections only if necessary
-	if err := initializeCollections(app); err != nil {
-		log.Fatal(err)
-	}
+	// Perform additional initialization after bootstrap
+	app.OnAfterBootstrap().Add(func(e *core.BootstrapEvent) error {
+		// Initialize pre-defined collections
+		if err := initializePredefinedCollections(app); err != nil {
+			return err
+		}
+
+		// Load search parameters from a JSON file
+		searchParamsBundle, err := loadSearchParameters("search-parameters.json")
+		if err != nil {
+			return err
+		}
+
+		// Initialize collections based on search parameters
+		if err := initializeCollections(app, searchParamsBundle); err != nil {
+			return err
+		}
+
+		return nil
+	})
 
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
