5cf16724212d2e2ee4fb485018e981ecc3592f73
Dokotela <grey.faulkenberry@mayjuun.com>
2024-10-02 15:06:09 -0400

    reorg for mobile

--- build/build_local.sh
+++ build/build_local.sh
@@ -4,7 +4,7 @@
 cd "$(dirname "$0")/.."
 
 # Clean up previous builds
-rm -f pocketfhir
+rm -f pocketfhir_server
 
 # Initialize Go modules in the root directory if go.mod does not exist
 if [ ! -f go.mod ]; then
@@ -16,10 +16,10 @@ go mod tidy
 
 # Build the PocketFHIR server for desktop and place the executable in the root directory
 echo "Building PocketFHIR for regular platform..."
-CGO_ENABLED=0 go build -o pocketfhir ./src
+CGO_ENABLED=0 go build -o pocketfhir_server ./src
 echo "Regular PocketFHIR build completed."
 
 # Start the PocketFHIR server in the background (for the regular platform)
-./pocketfhir serve
+./pocketfhir_server serve
 
 echo "PocketFHIR server started."
