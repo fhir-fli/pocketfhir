c7aa178873051ec6c14a4ded7f0a8ef1e1ede619
Dokotela <grey.faulkenberry@mayjuun.com>
2024-11-20 18:39:46 -0500

    I think that might have done it

--- pocketfhir/caddy.go
+++ pocketfhir/caddy.go
@@ -46,13 +46,13 @@ func StartCaddy(pbPort, httpPort, httpsPort, pbUrl, storagePath, ipAddress strin
 
 // CreateConfig generates a basic Caddy configuration to run a reverse proxy with HTTP and a static file server.
 func createConfig(pbPort, httpPort, httpsPort, pbUrl, storagePath, ipAddress string) *caddy.Config {
-	// Use storagePath for the log file paths
-	caddyDebugLogPath := fmt.Sprintf("%s/caddy_debug.log", storagePath)
-	caddyHTTPSDebugLogPath := fmt.Sprintf("%s/caddy_https_debug.log", storagePath)
+	// Define paths for certs and proxy logs
+	certsPath := fmt.Sprintf("%s/certs_access.log", storagePath)
+	pbProxyPath := fmt.Sprintf("%s/proxy_access.log", storagePath)
 	rootCertPath := fmt.Sprintf("%s/pki/authorities/local", storagePath)
 
 	// Generate the JSON configuration
-	jsonConfig := jsonConfig(pbPort, httpPort, httpsPort, pbUrl, storagePath, ipAddress, caddyDebugLogPath, caddyHTTPSDebugLogPath, rootCertPath)
+	jsonConfig := jsonConfig(pbPort, httpPort, httpsPort, pbUrl, storagePath, ipAddress, certsPath, pbProxyPath, rootCertPath)
 	log.Printf("Generated JSON Configuration: %s", jsonConfig)
 
 	// Parse the JSON into a caddy.Config struct
@@ -67,116 +67,200 @@ func createConfig(pbPort, httpPort, httpsPort, pbUrl, storagePath, ipAddress str
 	return &caddyConfig
 }
 
-func jsonConfig(pbPort, httpPort, httpsPort, pbUrl, storagePath, ipAddress, caddyDebugLogPath, caddyHTTPSDebugLogPath, rootCertPath string) string {
-	// JSON Configuration for Caddy with a basic file server
+func jsonConfig(pbPort, httpPort, httpsPort, pbUrl, storagePath, ipAddress, certsPath, pbProxyPath, rootCertPath string) string {
+	// Construct each part individually and combine them
+	loggingConfig := generateLoggingConfig(certsPath, pbProxyPath)
+	storageConfig := generateStorageConfig(storagePath)
+	httpAppConfig := generateHttpAppConfig(pbPort, httpPort, httpsPort, pbUrl, rootCertPath, ipAddress)
+	tlsConfig := generateTlsConfig(ipAddress)
+
+	// Combine the parts into the final JSON configuration
 	return fmt.Sprintf(`{
-        "logging": {
+        %s,
+        %s,
+        "apps": {
+            %s,
+            %s
+        }
+    }`, loggingConfig, storageConfig, httpAppConfig, tlsConfig)
+}
+
+// Generates the logging configuration section
+func generateLoggingConfig(certsPath, pbProxyPath string) string {
+	return fmt.Sprintf(`"logging": {
             "logs": {
-                "default": {
-                    "level": "DEBUG",
-                    "writer": {
-                        "output": "file",
-                        "filename": "%s"
-                    }
-                },
-                "http.access": {
-                    "level": "DEBUG",
+                "certs": {
                     "writer": {
                         "output": "file",
                         "filename": "%s"
                     },
                     "encoder": {
                         "format": "json"
-                    }
+                    },
+                    "include": [
+                        "http.log.access.certs"
+                    ]
+                },
+                "pb_proxy": {
+                    "writer": {
+                        "output": "file",
+                        "filename": "%s"
+                    },
+                    "encoder": {
+                        "format": "json"
+                    },
+                    "include": [
+                        "http.log.access.pb_proxy"
+                    ]
                 }
             }
-        },
-        "storage": {
+        }`, certsPath, pbProxyPath)
+}
+
+// Generates the storage configuration section
+func generateStorageConfig(storagePath string) string {
+	return fmt.Sprintf(`"storage": {
             "module": "file_system",
             "root": "%s"
-        },
-        "apps": {
-            "http": {
-                "http_port": %s,
-                "servers": {
-                    "srv_http": {
-                        "listen": [":%s"],
-                        "routes": [
-                            {
-                                "match": [{"host": ["%s"]}],
-                                "handle": [
-                                    {
-                                        "handler": "reverse_proxy",
-                                        "transport": {
-                                            "protocol": "http",
-                                            "read_timeout": 360000000000
-                                        },
-                                        "upstreams": [
-                                            {
-                                                "dial": "%s:%s"
-                                            }
-                                        ]
-                                    }
-                                ]
-                            },
-                            {
-                                "handle": [
-                                    {
-                                        "handler": "file_server",
-                                        "root": "%s",
-                                        "browse": true
-                                    }
-                                ]
-                            }
-                        ],
-                        "logs": {
-                            "logger_names": {
-                                "%s": ["log0"]
-                            }
-                        }
-                    },
-                    "srv_https": {
-                        "listen": [":%s"],
-                        "routes": [
-                            {
-                                "match": [{"host": ["%s"]}],
-                                "handle": [
-                                    {
-                                        "handler": "reverse_proxy",
-                                        "transport": {
-                                            "protocol": "http",
-                                            "read_timeout": 360000000000
-                                        },
-                                        "upstreams": [
-                                            {
-                                                "dial": "%s:%s"
-                                            }
-                                        ]
-                                    }
-                                ]
-                            }
-                        ],
-                        "logs": {
-                            "logger_names": {
-                                "%s": ["log1"]
-                            }
-                        }
-                    }
-                }
-            },
-            "tls": {
-                "automation": {
-                    "policies": [
+        }`, storagePath)
+}
+
+// Generates the HTTP application configuration section
+func generateHttpAppConfig(pbPort, httpPort, httpsPort, pbUrl, rootCertPath, ipAddress string) string {
+	httpServerConfig := generateHttpServerConfig(httpPort, ipAddress, rootCertPath)
+	httpsServerConfig := generateHttpsServerConfig(httpsPort, ipAddress, pbUrl, pbPort)
+	return fmt.Sprintf(`"http": {
+            "http_port": %s,
+            "servers": {
+                %s,
+                %s
+            }
+        }`, httpPort, httpServerConfig, httpsServerConfig)
+}
+
+// Generates the HTTP server configuration
+func generateHttpServerConfig(httpPort, ipAddress, rootCertPath string) string {
+	return fmt.Sprintf(`"srv1": {
+            "listen": [":%s"],
+            "routes": [
+                {
+                    "match": [{"host": ["%s"]}],
+                    "handle": [
                         {
-                            "issuers": [
+                            "handler": "subroute",
+                            "routes": [
                                 {
-                                    "module": "internal"
+                                    "handle": [
+                                        {
+                                            "handler": "vars",
+                                            "root": "%s"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "handle": [
+                                        {
+                                            "handler": "subroute",
+                                            "routes": [
+                                                {
+                                                    "handle": [
+                                                        {
+                                                            "handler": "rewrite",
+                                                            "strip_path_prefix": "/certs"
+                                                        }
+                                                    ]
+                                                },
+                                                {
+                                                    "handle": [
+                                                        {
+                                                            "browse": {},
+                                                            "handler": "file_server",
+                                                            "hide": [
+                                                                "./Caddyfile.fhirant"
+                                                            ]
+                                                        }
+                                                    ]
+                                                }
+                                            ]
+                                        }
+                                    ],
+                                    "match": [
+                                        {
+                                            "path": ["/certs/*"]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ],
+                    "terminal": true
+                }
+            ],
+            "logs": {
+                "logger_names": {
+                    "%s": [
+                        "certs"
+                    ]
+                }
+            }
+        }`, httpPort, ipAddress, rootCertPath, ipAddress)
+}
+
+// Generates the HTTPS server configuration
+// Generates the HTTP server configuration for reverse proxy with health checks
+func generateHttpsServerConfig(httpsPort, ipAddress, pbUrl, pbPort string) string {
+	return fmt.Sprintf(`"srv_https": {
+            "listen": [":%s"],
+            "routes": [
+                {
+                    "match": [{"host": ["%s"]}],
+                    "handle": [
+                        {
+                            "handler": "reverse_proxy",
+                            "transport": {
+                                "protocol": "http",
+                                "read_timeout": 360000000000
+                            },
+                            "health_checks": {
+                                "active": {
+                                    "interval": 10000000000,
+                                    "timeout": 2000000000,
+                                    "uri": "/api/health"
+                                }
+                            },
+                            "upstreams": [
+                                {
+                                    "dial": "%s:%s"
                                 }
                             ]
                         }
                     ]
                 }
+            ],
+            "logs": {
+                "logger_names": {
+                    "%s": ["log1"]
+                }
             }
-        }
-    }`, caddyDebugLogPath, caddyHTTPSDebugLogPath, storagePath, httpPort, httpPort, ipAddress, pbUrl, pbPort, storagePath, ipAddress, httpsPort, ipAddress, pbUrl, pbPort, ipAddress)
+        }`, httpsPort, ipAddress, pbUrl, pbPort, ipAddress)
+}
+
+// Generates the TLS automation configuration
+func generateTlsConfig(ipAddress string) string {
+	return fmt.Sprintf(`"tls": {
+            "automation": {
+                "policies": [
+                    {
+                        "subjects": [
+                            "%s"
+                        ],
+                        "issuers": [
+                            {
+                                "module": "internal"
+                            }
+                        ]
+                    }
+                ]
+            }
+        }`, ipAddress)
 }
